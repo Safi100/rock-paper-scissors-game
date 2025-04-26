f967508d69a3cdb69cb2872cd77517b53071a213
Safi100 <safinafi12@gmail.com>
2025-04-26 03:41:12 +0300

    add score to local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Rules from "./components/Rules";
 import Header from "./components/Header";
 import Game from "./components/Game";
 
 function App() {
+  const savedScore = parseInt(localStorage.getItem("score"), 10);
+  const [score, setScore] = useState(savedScore || 0);
   const [openRules, setOpenRules] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("score", score);
+  }, [score]); // runs only when score changes
+
   return (
     <>
       <div className="App">
-        <Header />
-        <Game setOpenRules={setOpenRules} />
+        <Header score={score} />
+        <Game setOpenRules={setOpenRules} setScore={setScore} />
         <Rules openRules={openRules} setOpenRules={setOpenRules} />
       </div>
     </>
